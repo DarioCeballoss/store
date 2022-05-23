38345ecaabb9b7e82154d30a47401292f3607df2
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-05-23 18:59:02 -0300

    Cree una componente y cree un contrato de interfas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DominicodeComponent } from './pages/dominicode/dominicode.component';
+
 
 const routes: Routes = [
-  {path: 'dominicode', component: DominicodeComponent},
+
+  { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
+
+  { path: 'product', loadChildren: () => import('./pages/products/product/product.module').then(m => m.ProductModule) },
   {path: '**', redirectTo: '', pathMatch:'full' }
 ];
 
---
+++ src/app/pages/products/interfaces/product.interface.ts
@@ -0,0 +1,8 @@
+export interface product{
+    id: number;
+    name:string;
+    price:number;
+    description: string;
+    categoryID:number;
+    stock: number;
+}
