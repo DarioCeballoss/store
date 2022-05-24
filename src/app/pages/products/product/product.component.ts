0670acc4ce4305bb4d2dc8e3549f484dbb685307
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-05-23 21:45:37 -0300

    Agrega funcionalidad al boton, para que se comunique desde el hijo al padre 1

--- src/app/pages/products/product/product.component.ts
+++ src/app/pages/products/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
 import { Product } from '../interfaces/product.interface';
 
 @Component({
@@ -9,8 +9,14 @@ import { Product } from '../interfaces/product.interface';
 export class ProductComponent implements OnInit {
 @Input() product!: Product;
   constructor() { }
+  @Output() addToCartClick= new EventEmitter<Product>();
 
   ngOnInit(): void {
+  }
+
+  onClick():void{
+console.log('Click', this.product);
+this.addToCartClick.emit(this.product);
   }
 
 }
